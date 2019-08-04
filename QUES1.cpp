0d8c57d8b817cb1d2f157574e53a722a2040cfba
bikash1317 <50355217+bikash1317@users.noreply.github.com>
2019-08-04 23:19:24 +0530

    Update QUES1.cpp

--- QUES1.cpp
+++ QUES1.cpp
@@ -2,7 +2,7 @@
 #include <conio.h>
 void main ()
 {
-int n,i=0;
+int n,i=0,c=0;
 cout<< "enter any positive integer number";
 cin>>n;
 for(i=0;i>n;i++)
@@ -10,6 +10,8 @@ for(i=0;i>n;i++)
 if(n%3==0 || n%5==0 )
 {
 cout<<n<<" ";
+  c=c+n;
 }
 }
+  cout<<"the sum of all the multiples of 3 and 5 are = "<< c;
 }
