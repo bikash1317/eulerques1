0501f8b449a1108763fc281aeea8922c576f100a
bikash1317 <50355217+bikash1317@users.noreply.github.com>
2019-08-05 18:32:54 +0530

    Update QUES2.cpp

--- QUES2.cpp
+++ QUES2.cpp
@@ -1,2 +1,14 @@
 # include<iostream.h>
 # include <conio.h>
+void main()
+{
+  int a=0,b=1,c=0,sum=0,i;
+  for(i=0,i<n;i++)
+  {
+    c=a+b;
+    a=b;
+    b=c;
+    if(c%2==0)
+      sum=sum+c;
+  }
+}
