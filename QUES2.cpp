ee231742db9e6e4189f8bc47f5256bced428fade
bikash1317 <50355217+bikash1317@users.noreply.github.com>
2019-08-05 20:03:03 +0530

    Update QUES2.cpp

--- QUES2.cpp
+++ QUES2.cpp
@@ -2,7 +2,9 @@
 # include <conio.h>
 void main()
 {
-  int a=0,b=1,c=0,sum=0,i;
+  int a=0,b=1,c=0,sum=0,i,n;
+  cout<<"enter the number to be inputed";
+  cin>>n;
   for(i=0,i<n;i++)
   {
     c=a+b;
