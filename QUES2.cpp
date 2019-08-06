031ff6e54b3258a994fc9ec64ac399ba8f31d2c6
bikash1317 <50355217+bikash1317@users.noreply.github.com>
2019-08-06 20:23:35 +0530

    Update QUES2.cpp

--- QUES2.cpp
+++ QUES2.cpp
@@ -1,9 +1,9 @@
-# include<iostream.h>
-# include <conio.h>
-void main()
-{
-  int a=0,b=1,c=0,sum=0,i,n;
-  for(i=0;i<4000000;i++)
+#include <iostream.h>
+#include<conio.h>
+
+void main() {
+	 int a=0,b=1,c=0,sum=0,n;
+  for(c=0;c<4000000;)
   {
     c=a+b;
     a=b;
@@ -11,6 +11,6 @@ void main()
     if(c%2==0)
       sum=sum+c;
   }
-  cout<<sum;
+cout<<sum;
+	
 }
-
